f439b2054a30c060a71d62a95e113c917b0524ae
sobuj191 <mdsobujcmt@gmail.com>
2023-10-17 14:39:48 +0600

    Service

---
+++ src/Booking.Infrastructure/Messages/MessageService.cs
@@ -0,0 +1,24 @@
+﻿using Booking.Application.Contacts.Infrastructure;
+using Booking.Application.Models;
+using QuickMailer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booking.Infrastructure.Messages
+{
+    public class MessageService : IMessageService
+    {
+        public async Task<bool> SendMessageAsync(Message message)
+        {
+            Email email = new Email();
+            if (email.IsValidEmail(message.To))
+            {
+                return email.SendEmail(message.To, "", "", message.Subject, message.Body);
+            }
+            return false;
+        }
+    }
+}
